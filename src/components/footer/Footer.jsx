aa1cf909a277d01412b6cf69ecdba7d40d775e9e
Saheed Tajudeen <saheed.taaj@gmail.com>
2022-10-28 07:51:48 +0100

    feat: Complete footer section and mobile version

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,15 @@ import './footer.css'
 const Footer = () => {
   return (
     <footer>
-        
+      <a class="button zuri-logo" href='https://internship.zuri.team/hngi9'>
+        <img src="/img/zuri-logo.png" alt="zuri-logo" />
+      </a>
+      <a className="button hng-internship" href='https://hng9.notion.site/Front-End-Stage-1-Task-cdaf503a796b4a5a85c5883a0a6163db'>
+        HNG Internship 9 FrontEnd Task
+      </a>
+      <a class="button ingressive-logo" href='https://ingressive.org/'>
+        <img src="/img/ingressive.png" alt="ingressive-logo" />
+      </a>
     </footer>
   )
 }
